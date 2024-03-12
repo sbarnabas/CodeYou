2738b18d6c5af3e7a146d4855066cbeb65ba1c3c
Sandy Barnabas <sbarnabas@gmail.com>
2024-03-11 23:11:41 -0400

    Added weather fetching and parsing

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using WeatherUtilities;
+internal class Program
+{
+    private static  void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        WeatherUtilities.WeatherFetcher w= new WeatherUtilities.WeatherFetcher();
+        w.getForecastDataForZip("45069");
+        
+    }
+}
